8d0e82d0075b8ad34c88d0522d01a29856087e33
Wagner Carestini <wcarestini@gmail.com>
2021-05-04 17:35:01 -0300

    Configuraçao do swagger e atualizaçao do readme.

---
+++ src/doc/swagger.js
@@ -0,0 +1,34 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+    info: {
+        version: '1.0.0',
+        title: 'Curtas.Tv-API',
+        description: 'API do Curtas.TV'
+    },
+    host: `${process.env.SERVER_URL || 'localhost'}:${process.env.SERVER_PORT || 3333}`,
+    schemes: ['http'],
+    tags: [
+        {
+            name: 'movie',
+            description: 'Endpoints movies.'
+        }
+    ],
+    definitions: {
+        PopularMovies: [{
+            id: 'c9a1f40a-65bc-420a-abac-4e140396d526',
+            title: 'Movie Title',
+            posterUrl: "https://m.media-amazon.com/images/M/MV.jpg"
+        }],
+        Error500: {
+            status: 'error',
+            statusCode: 500,
+            message: 'ErrorMessage'
+        }
+    }
+}
+
+const outputFile = 'src/doc/swagger-output.json'
+const endpointsFiles = ['src/routes.js']
+
+swaggerAutogen(outputFile, endpointsFiles, doc)
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,33 @@
 import { Router } from 'express'
 
+const swaggerFile = require('./doc/swagger-output.json')
+const swaggerUi = require('swagger-ui-express')
+
 import MoviesController from './controllers/MoviesController'
 
 const routes = Router()
 
 const moviesController = new MoviesController()
 
-routes.get('/destaques', moviesController.findAllMaxPopularity)
+routes.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+routes.get('/destaques', async (request, response) => {
+    // #swagger.tags = ['movie']
+    // #swagger.description = 'Endpoint para obter os curtas em destaque.'
+    // #swagger.parameters['limitOf'] = { description: 'Limite para quantidade de curtas retornado.', type: 'integer'}
+
+    /** #swagger.responses[200] = { 
+         schema: { $ref: "#/definitions/PopularMovies" },
+        description: 'Lista de curtas em destaque encontrados.' 
+     } 
+    */
+
+    /** #swagger.responses[5xx] = {
+        schema: { $ref: "#/definitions/Error500" },
+        description: 'Erro interno.' 
+     }
+     */
+    return await moviesController.findAllMaxPopularity(request, response)
+})
 
 export { routes }
